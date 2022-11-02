560030b3607dc280d0b7d939da9ef7acecaf14c9
subhan tariq <subhantariq257@gmail.com>
2022-11-02 05:19:07 -0700

    initial commit

---
+++ reverse a number.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int number{}, n{}, num{};
+	cout << "Enter a number: ";
+	cin >> number;
+	while (number)
+	{
+		num = (number % 10) + (num * 10);
+		number = number / 10;
+	}
+	cout << num;
+}
